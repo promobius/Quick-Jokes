ad2b60bda7f6440cc4565d3863e5127a60b793fb
promobius <matt.huebert@gmail.com>
2020-12-10 11:47:18 -0600

    working on it

---
+++ Quick Jokes/DadJokes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quick_Jokes
+{
+    public class DadJokes
+    {
+        public string setup { get; set; }
+        public string punchline { get; set; }
+    }
+}
---
+++ Quick Jokes/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace Quick_Jokes
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private int count = 0;
+        private int count2 = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void IncrementCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            response1.Text = count.ToString();
+        }
+        private void IncrementCounterClicked2(object sender, EventArgs e)
+        {
+            count2++;
+            response2.Text = count2.ToString();
+        }
+    }
+}
+
